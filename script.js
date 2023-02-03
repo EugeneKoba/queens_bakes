1b7641f9c07c66cf729e376381db83c0bf120c9d
Eugene Annan <eugeneannan@Eugenes-MacBook-Pro.local>
2023-02-03 15:42:23 +0000

    Adding index.html, styles.css & script.js
    Adding .html .css .js files

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// Customer Reviews stored in Objects
+
+const customerReviews = [
+    {
+        customerId: 1,
+        customerName: "Eugene Annan",
+        customerJob: "Operations Assistant",
+        customerDescription: "This service is amazing! I would definitely recommend this to my other colleagues & friends!",
+        customerImg: "Linked-In_Image.jpeg",
+    }, 
+    {
+        customerId: 2,
+        customerName: "Stacey Earnhart",
+        customerJob: "Nanny",
+        customerDescription: "This service is amazing! I would definitely recommend this to my other colleagues & friends!",
+        customerImg: "Linked-In_Image.jpeg",
+    },
+    {
+        customerId: 3,
+        customerName: "Bartholomew Jackson",
+        customerJob: "Secondary School Teacher",
+        customerDescription: "This service is amazing! I would definitely recommend this to my other colleagues & friends!",
+        customerImg: "Linked-In_Image.jpeg",
+    }
+]   
